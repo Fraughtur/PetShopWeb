a6e1816c3690828d257641ffef071bf8ec5a951e
Fraughtur <Fraughtur.freraightter@hotmail.com>
2022-05-20 09:59:38 -0400

    cosa x y agregacion de nueva rama

--- PetShop/core/urls.py
+++ PetShop/core/urls.py
@@ -1,6 +1,8 @@
+from unicodedata import category, name
 from django.urls import path
 from .views import home
 
 urlpatterns = [
-    path('',home,name="home")
+    path('',home,name="home"),
+    path('Petshop/category/', category, name="category"),
 ]
