aea731d18329a1a9b6c2a860d3f4e383e8020c16
Your Name <you@example.com>
2022-06-06 12:19:46 -0400

    Espero que sea algo bueno, comit de algo que no me acuerdo

--- PetShop/core/admin.py
+++ PetShop/core/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Producto,Categoria
+from .models import Producto,Categoria, Usuario, Socio
 # Register your models here.
 
 admin.site.register(Producto)
 admin.site.register(Categoria)
-
+admin.site.register(Usuario)
+admin.site.register(Socio)
